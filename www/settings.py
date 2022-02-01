975d85ffb97437f0ff330cadd174f2a37fc47cc6
Claire Chung <35644336+haewonchung@users.noreply.github.com>
2022-02-02 06:11:41 +0900

    Connect static and templates

--- www/settings.py
+++ www/settings.py
@@ -119,8 +119,6 @@ USE_TZ = True
 
 # STATIC_URL = 'static/'
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static', ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -139,3 +137,11 @@ AWS_ACCESS_KEY_ID = secrets['AWS']['ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = secrets['AWS']['SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = secrets['AWS']['STORAGE_BUCKET_NAME']
 AWS_DEFAULT_ACL = 'public-read'  # 올린 파일을 누구나 읽을 수 있게 지정합니다!
+
+#
+AWS_S3_REGION_NAME = 'ap-northeast-2'
+AWS_LOCATION = 'static'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.%s.amazonaws.com' % (AWS_STORAGE_BUCKET_NAME, AWS_S3_REGION_NAME)
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_DIRS = [BASE_DIR / 'static', ]
+print(f'here{STATICFILES_DIRS}')
