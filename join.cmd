20ff069215b7cdc9a3fe412daf7033f427e29419
mijikim <meeji222@naver.com>
2022-02-04 16:55:26 +0900

    recommend logout

--- join.cmd
+++ join.cmd
@@ -7,7 +7,7 @@ set project_name= world wonder wine
 @REM Server Information.
 echo --------------------------------------------
 echo.
-echo 프로젝트 참가 인원 : 정혜원, 김미지, 유성훈, 조성현, 유민우 
+echo 프로젝트 참가 인원 : 정혜원, 김미지, 유성훈, 조성현, 유민호
 echo 프로젝트 기한 일자 : 2022-01-26 ~ 2022-02-09
 echo 프로젝트 이름 : %project_name%
 echo mysql 버전 : 아직 안적음.
