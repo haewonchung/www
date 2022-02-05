5881a891095d04ab397a1352b3b60a872e19bb6e
Claire Chung <35644336+haewonchung@users.noreply.github.com>
2022-02-05 17:00:26 +0900

    Modified views, urls, and base nav link
    - Refactoring
       - 함수 이름, urls pattern, html 이름이 제각각이라 최대한 직관적으로 통일
       - wine_recommend, wine_all, wine_detail 로 구성
    - My-pic

--- recommendation/views.py
+++ recommendation/views.py
@@ -8,17 +8,29 @@ def home(request):
     user = request.user.is_authenticated
     print(user)
     if user:  # 사용자가 있으면 main으로
-        return redirect('/main')
+        return redirect('/wine_recommend')
     else:  # 사용자가 없으면 로그인화면으로
         return redirect('/sign-in')
 
 
-def main(request):
+@login_required
+def wine_recommend(request):
     wines = Wine.objects.filter(region='Napa Valley')  # 일단은 유저 추천 대신 필터로 적용해둠
-    return render(request, 'recommendation/recommend.html', {'wines': wines})
+    return render(request, 'recommendation/wine_recommend.html', {'wines': wines})
+
+
+def wine_all(request):
+    if request.method == "GET":
+        return render(request, "recommendation/wine_all.html")
 
 
 @login_required()
 def wine_detail(request, id):
     if request.method == "GET":
         return render(request, "recommendation/wine_detail.html")
+
+
+@login_required
+def my_pic(request):
+    if request.method == "GET":
+        return render(request, "recommendation/my_pic.html")
