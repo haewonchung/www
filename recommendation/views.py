752623b7be6fd37d862d7e1d7ce151e47792a0d6
Claire Chung <35644336+haewonchung@users.noreply.github.com>
2022-02-09 09:37:14 +0900

    Modified save toggle based on current path
    recommendation:wine-save-toggle 에 check_page 추가해서 current path 체크해서 redirect

--- recommendation/views.py
+++ recommendation/views.py
@@ -11,7 +11,7 @@ def home(request):
     if user:  # 사용자가 있으면 main으로
         return redirect('/wine-recommend')
     else:  # 사용자가 없으면 로그인화면으로
-        return redirect('/sign-in')
+        return redirect('/wine-all')
 
 
 @login_required
@@ -19,32 +19,29 @@ def wine_recommend(request):
     me = request.user
     if me.surveyed:
         recommend_wine = WineRecommend.objects.filter(user=me)
-        print(recommend_wine)
+        # print(recommend_wine)
         my_list = []
         for wine in recommend_wine:
             my_list.append(wine.wine_id)
-        print(my_list)
+        # print(my_list)
         wines = Wine.objects.all()
         return render(request, 'recommendation/wine_recommend.html', {'list': my_list, 'wines': wines})
     else:
         return redirect('/prefer')
 
 
-@login_required
 def wine_all(request):
     wines = Wine.objects.all()  # wines 전체
     if request.method == "GET":
         return render(request, "recommendation/wine_all.html", {'wines': wines})
 
 
-@login_required
 def wine_detail(request, id):
     wine = Wine.objects.get(id=id)  # wine id로 선별
     wine_profile = WineProfile.objects.get(wine=wine)
     wine_id = wine.yturl[-11:]   # youtube video id
     food_list = [x.food for x in wine.winefood_set.all()]
-    print('food_list', food_list)
-    # print(wine_id)
+    # print('food_list', food_list)
     if request.method == "GET":
         return render(request, "recommendation/wine_detail.html",
                       {'wine': wine, 'wine_profile': wine_profile, 'wine_id': wine_id, 'food_list': food_list})
@@ -60,7 +57,9 @@ def my_pick(request):
 
 
 # 와인 mypick 저장 기능
-def wine_save_toggle(request, wine_id):
+@login_required
+def wine_save_toggle(request, wine_id, check_page):
+    print(check_page)
     wine = get_object_or_404(Wine, id=wine_id)
     me = get_object_or_404(User, id=request.user.id)
 
@@ -75,7 +74,7 @@ def wine_save_toggle(request, wine_id):
         wine.saved_count += 1
         wine.save()
 
-    return redirect('recommendation:wine-recommend')
+    return redirect(f'recommendation:{check_page}')
 
 
 # 검색 기능
@@ -88,7 +87,7 @@ def search(request):
         products = Wine.objects.all().filter(
             Q(name__icontains=query) | Q(type__icontains=query) | Q(region__icontains=query) | Q(
                 country__icontains=query) | Q(primary_flavors__icontains=query)).distinct()
-        print('products', products)
+        # print('products', products)
     return render(request, 'recommendation/search_result.html', {'query': query, 'wines': products})
 
 
