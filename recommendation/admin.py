e339d1222c95fffb9262330fbad9e09009f6d6a3
Claire Chung <35644336+haewonchung@users.noreply.github.com>
2022-03-17 04:18:00 -0400

    first commit

--- recommendation/admin.py
+++ recommendation/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Wine, WineProfile, Food
-
+from .models import Wine, WineProfile, Food, WineRecommend, WineFood
 
 # Register your models here.
 admin.site.register(Wine)
 admin.site.register(WineProfile)
+admin.site.register(WineRecommend)
+admin.site.register(WineFood)
 admin.site.register(Food)
 
